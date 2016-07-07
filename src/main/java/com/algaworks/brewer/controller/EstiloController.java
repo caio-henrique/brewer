910c077392b8656bc882c47c4f496c8ed4f41c05
Caio Henrique <caiohenrique.cs@gmail.com>
2016-07-07 14:30:45 -0300

    Salvando o estilo com Ajax

--- src/main/java/com/algaworks/brewer/controller/EstiloController.java
+++ src/main/java/com/algaworks/brewer/controller/EstiloController.java
@@ -3,15 +3,20 @@ package com.algaworks.brewer.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.algaworks.brewer.model.Estilo;
 import com.algaworks.brewer.service.EstiloService;
+import com.algaworks.brewer.service.exception.NomeEstiloJaCadastradoException;
 
 @Controller
 public class EstiloController {
@@ -32,8 +37,39 @@ public class EstiloController {
 			return novo(estilo);
 		}
 		
-		this.estiloService.salvar(estilo);
-		attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso");
+		try {
+			
+			this.estiloService.salvar(estilo);
+			attributes.addFlashAttribute("mensagem", "Estilo salvo com sucesso");
+		}
+		
+		catch(NomeEstiloJaCadastradoException exception) {
+			
+			result.rejectValue("nome", exception.getMessage(), exception.getMessage());
+			return novo(estilo);
+		}
+		
 		return new ModelAndView("redirect:/estilos/novo");
+	}
+	
+	@RequestMapping(value = "/estilos", method = RequestMethod.POST, consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public @ResponseBody ResponseEntity<?> salvar(@RequestBody @Valid Estilo estilo, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			
+			return ResponseEntity.badRequest().body(result.getFieldError("nome").getDefaultMessage());
+		}
+		
+		try {
+			
+			estilo = estiloService.salvar(estilo);
+		} 
+		
+		catch (NomeEstiloJaCadastradoException exception) {
+			
+			return ResponseEntity.badRequest().body(exception.getMessage());
+		}
+		
+		return ResponseEntity.ok(estilo);
 	}
 }
--- src/main/java/com/algaworks/brewer/service/EstiloService.java
+++ src/main/java/com/algaworks/brewer/service/EstiloService.java
@@ -1,11 +1,14 @@
 package com.algaworks.brewer.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.brewer.model.Estilo;
 import com.algaworks.brewer.repository.Estilos;
+import com.algaworks.brewer.service.exception.NomeEstiloJaCadastradoException;
 
 @Service
 public class EstiloService {
@@ -14,8 +17,15 @@ public class EstiloService {
 	private Estilos estilos;
 	
 	@Transactional
-	public void salvar(Estilo estilo) {
+	public Estilo salvar(Estilo estilo) {
 		
-		this.estilos.save(estilo);
+		Optional<Estilo> estiloOptional = this.estilos.findByNomeIgnoreCase(estilo.getNome());
+		
+		if(estiloOptional.isPresent()){
+			
+			throw new NomeEstiloJaCadastradoException("Nome do estilo já cadastrado");
+		}
+		
+		return this.estilos.saveAndFlush(estilo);
 	}
 }
